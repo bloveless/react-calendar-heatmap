b25af9b4ba5b228a1ba5ff421a512dc344e0c389
Brennon Loveless <brennon.loveless@gmail.com>
2018-08-26 12:05:43 -0600

    Added calendar heatmap.

--- docs/code/src/index.js
+++ docs/code/src/index.js
@@ -1,6 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './pages/Home';
 
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App.js";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Home />, document.getElementById('root'));
